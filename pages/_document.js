12d801209b7c1e26746b6d6ba9ee2bd2c45052d8
Shibashish Mohapatra <shibashishmohapatra1@gmail.com>
2023-07-09 18:49:08 +0530

    document change

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 export default function Document() {
   return (
     <Html lang="en">
@@ -6,6 +7,20 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8651032257595693"
+     crossorigin="anonymous"></Script>
+  {/* Google Ads */}
+  <ins
+    className="adsbygoogle"
+    style={{ display: "block" }}
+    data-ad-client="ca-pub-8651032257595693"
+    data-ad-slot={2870239440}
+    data-ad-format="auto"
+    data-full-width-responsive="true"
+  />
+  <Script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</Script>
       </body>
     </Html>
   )
